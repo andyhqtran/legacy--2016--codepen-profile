212df404dbdb4afe4dbd2325778d5726684c2824
Andy Tran <andyhqtran@gmail.com>
2016-06-14 21:25:27 -0700

    Initial React + Webpack project.

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { render } from 'react-dom';
+
+/**
+ * Internal dependencies
+ */
+import './style.scss';
+
+const App = () => <div>App</div>;
+
+render(<App />, document.getElementById('root'));
