5410a59543f709227e4bd31253cca2e12443983f
Andy Tran <andyhqtran@gmail.com>
2016-06-18 16:03:13 -0700

    Updated Button, SuggestionsCard component. Added in suggested users.

--- app/components/SuggestionsCard/index.jsx
+++ app/components/SuggestionsCard/index.jsx
@@ -8,6 +8,8 @@ import $ from 'jquery';
  * Internal dependencies
  */
 import './style.scss';
+import EyeIcon from '../../assets/img/icon-eye.svg';
+import users from './helpers/users';
 import Card, { CardHeader, CardContent, CardFooter } from '../Card';
 import Button from '../Button';
 
@@ -28,6 +30,7 @@ class SuggestionsCard extends Component {
 
     this.state = {
       more: false,
+      users,
     };
 
     this._handleMore = this._handleMore.bind(this);
@@ -39,12 +42,48 @@ class SuggestionsCard extends Component {
     });
   }
 
+  _renderUser(user, key) {
+    if (!this.state.more && key >= 6 || key >= 10) {
+      return false;
+    }
+
+    return (
+      <div
+        key={`suggestion-user-${key}`}
+        className="suggestions__user"
+      >
+        <div className="suggestions__avatar">
+          <img src={user.image} alt={user.name} />
+        </div>
+        <div className="suggestions__info">
+          <a href={`http://codepen.io/${user.username}`}>
+            <span>{user.name}</span>
+          </a>
+          <a href={`http://codepen.io/${user.username}`}>
+            <span>{user.username}</span>
+          </a>
+        </div>
+
+        <a href={`http://codepen.io/${user.username}`}>
+          <Button
+            outline
+            primary
+          >
+            <div className="suggestions__icon" dangerouslySetInnerHTML={{ __html: EyeIcon }} />
+          </Button>
+        </a>
+      </div>
+    );
+  }
+
   render() {
     return (
       <Card className="card--suggestions">
         <CardHeader>Suggestions for You</CardHeader>
         <CardContent>
-
+          <div className="suggestions">
+            {this.state.users.map(this._renderUser.bind(this))}
+          </div>
         </CardContent>
         <CardFooter>
           <Button
