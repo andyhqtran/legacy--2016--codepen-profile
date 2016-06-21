0731def5aa416473f751cb5206341230de87ef2f
Andy Tran <andyhqtran@gmail.com>
2016-06-20 19:00:33 -0700

    Updated Logo component.

--- app/components/Logo/index.jsx
+++ app/components/Logo/index.jsx
@@ -8,6 +8,7 @@ import cx from 'classnames';
  * Internal dependencies
  */
 import './style.scss';
+import CodePenLogo from '../../assets/img/logo.svg';
 
 /**
  * Local variables
@@ -29,33 +30,15 @@ const Logo = (props) => {
   let classes = cx('logo', props.className);
 
   return (
-    <svg
+    <div
       className={classes}
-      fill="none"
-      width={props.width}
-      height={props.height}
-      stroke={props.color}
-      strokeLinejoin="round"
-      style={props.style}
-      viewBox="0 0 4114 743"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        strokeWidth="70"
-        d="M402.5 199.5a223.8 223.8 0 1 0 0
-          343m1148.5-380V580h139a1 1 0 0 0 0-417.5zm896-.5h-290v418h290m-290-209h197m339
-          244V162h151a1 1 0 0 1 0
-          209h-151m808-209h-290v418h290m-290-209h197m672-243v452l-349-417v452"
-      />
-      <path
-        strokeWidth="64"
-        d="M1286 258L947 31.5 608
-          258v227l339 226 339-226zM947
-          35v223m0 227v226M608
-          258v227l339-227 339
-          227V258L947 485z"
-      />
-    </svg>
+      style={Object.assign({
+        width: props.width,
+        height: props.height,
+        stroke: props.color,
+      }, props.style)}
+      dangerouslySetInnerHTML={{ __html: CodePenLogo }}
+    />
   );
 };
 
